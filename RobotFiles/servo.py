ded1258a63e29af6036d589aed5c07728140052f
UselessBullets <mag085@email.latech.edu>
2023-04-19 20:30:56 -0500

    Update servo.py

--- RobotFiles/servo.py
+++ RobotFiles/servo.py
@@ -24,50 +24,43 @@ import time
 
 # Sets up output pins for communication with arduino
 triggerPin = 4
-dataPin0 = 17
-dataPin1 = 18
-dataPin2 = 22
-dataPin3 = 23
+dataPins = [17, 18, 22, 23]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(triggerPin, GPIO.OUT)
-GPIO.setup(dataPin0, GPIO.OUT)
-GPIO.setup(dataPin1, GPIO.OUT)
-GPIO.setup(dataPin2, GPIO.OUT)
-GPIO.setup(dataPin3, GPIO.OUT)
+[GPIO.setup(pin, GPIO.OUT) for pin in dataPins]
 
 def transmit(direction = 0, speed = 0, timer = 0, pulseWidth: float = 12/1000):
+  """Sends instructions to the arduino"""
+  direction_bin = bin(direction + 1024)[-3:] # Get the three bits for direction
+  speed_bin = bin(speed + 1024)[-5:] # Get the 5 bits for speed
+  timer_bin = bin(timer + 1024)[-8:] # Get the 8 bits for duration
 
-  direction_bin = bin(direction + 1024)[-3:]
-  speed_bin = bin(speed + 1024)[-5:]
-  timer_bin = bin(timer + 1024)[-8:]
-
-  #print(timer_bin,speed_bin,direction_bin)
-
+  # Combine bits into 2 byte list
   data = [int(i) for i in str(direction_bin)[::-1]] + [int(i) for i in str(speed_bin)[::-1]] + [int(i) for i in str(timer_bin)[::-1]]
-  dataSplit = []
-  for i in range(0, len(data)//4):
-    dataSplit.append([\
-      data[0 + 4*i],
-      data[1 + 4*i],
-      data[2 + 4*i],
-      data[3 + 4*i]])
 
-  print(dataSplit)
+  # Split data into 4 bit segments
+  dataSplit = [] 
+  for i in range(0, len(data)//4):
+    dataSplit.append([data[j + 4*i] for j in range(4)])
+
+  # Send interrupt to the arduino to start data transfer process
   GPIO.output(triggerPin, GPIO.HIGH)
   time.sleep(pulseWidth/2)
   GPIO.output(triggerPin, GPIO.LOW)
+
+  # Transmit data
   for halfByte in dataSplit:
-    GPIO.output(dataPin0, halfByte[0])
-    GPIO.output(dataPin1, halfByte[1])
-    GPIO.output(dataPin2, halfByte[2])
-    GPIO.output(dataPin3, halfByte[3])
+    for i in range(4):
+      GPIO.output(dataPins[i], halfByte[i])
     time.sleep(pulseWidth)
-  GPIO.output(dataPin0, GPIO.LOW)
+  
+  # Set all pins to low after transmission
+  for pin in dataPins:
+    GPIO.output(pin, GPIO.LOW)
 
 def stop() -> None:
   """Tells the servos to stop"""
-
   transmit(direction=0, speed=0, timer=0)
 
 def forward(velocity:int = 3, time:float = 0) -> None:
