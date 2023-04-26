238118e87cf6d5ce7e820e8312edad8c90740c6e
JakeTouchet <jake.t.touchet@gmail.com>
2023-04-26 09:59:08 -0500

    Set running true

--- ObjectDetection/detect.py
+++ ObjectDetection/detect.py
@@ -39,7 +39,7 @@ def main(args):
 
     TURN_THRESH = 0.2
 
-    running = False
+    running = True
 
     # Connect to RabbitMQ ########################################################
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='138.47.119.55', credentials=pika.PlainCredentials('admin1', 'admin1')))
