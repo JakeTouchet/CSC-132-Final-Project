e903dc7e39f8cc3a68df8ebd95b4c7beb21dc692
UselessBullets <mag085@email.latech.edu>
2023-04-27 15:02:46 -0500

    Call Echo test

--- RobotFiles/SerialComm/SerialComm.ino
+++ RobotFiles/SerialComm/SerialComm.ino
@@ -1,28 +1,12 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
 }
+int n = 0;
 void loop() {
   Serial.println("Hello from Arduino!");
   delay(1000);
-  int n = Serial.read() - '0';
-  allOff();
-  if (n % 3 == 0) {
-    digitalWrite(13,1);
-  }
-  else if (n % 3 == 1) {
-    digitalWrite(12,1);
-  }
-  else if (n % 3 == 2) {
-    digitalWrite(11,1);
-  }
-  Serial.println(n);
-}
-
-void allOff(){
-  digitalWrite(13,0);
-  digitalWrite(12,0);
-  digitalWrite(11,0);
+  if (Serial.available())
+    n = Serial.read();
+  if (Serial.availableForWrite())
+    Serial.println(n);
 }
