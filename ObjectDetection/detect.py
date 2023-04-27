977a61528cbffedd31a29b1ab16f853fee8f8873
JakeTouchet <jake.t.touchet@gmail.com>
2023-04-27 17:41:09 -0500

    moveuntil

--- ObjectDetection/detect.py
+++ ObjectDetection/detect.py
@@ -105,7 +105,7 @@ def main(args):
                     timedTurn(x_dist*2)
                 else:
                     start_time = time.time()
-                    changeDistanceThreshold(0.35)
+                    moveUntil(0.5)
                     print("Stop: "+str(ultraDistance()))
                     running = False
             else:
