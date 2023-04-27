26585e642ef0f0bf6d9bc8cb628ead14a0e6d058
UselessBullets <mag085@email.latech.edu>
2023-04-27 14:35:13 -0500

    Data update

--- RobotFiles/SerialComm/SerialComm.ino
+++ RobotFiles/SerialComm/SerialComm.ino
@@ -1,9 +1,28 @@
 void setup() {
   Serial.begin(9600);
+  setup(13, OUTPUT);
+  setup(12, OUTPUT);
+  setup(11, OUTPUT);
 }
 void loop() {
   Serial.println("Hello from Arduino!");
   delay(1000);
   int n = Serial.read() - '0';
+  allOff();
+  if n % 3 == 0{
+    digitalWrite(13,1);
+  }
+  else if n % 3 == 1{
+    digitalWrite(12,1);
+  }
+  else if n % 3 == 2{
+    digitalWrite(11,1);
+  }
   Serial.println(n);
+}
+
+void allOff(){
+  digitalWrite(13,0);
+  digitalWrite(12,0);
+  digitalWrite(11,0);
 }
--- RobotFiles/serialComm.py
+++ RobotFiles/serialComm.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     while True:
-        ser.write(str(5).encode())
+        ser.write(input().strip().encode())
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
