02f128dad26d5debd22a2e602c91a42e8c39cd63
UselessBullets <mag085@email.latech.edu>
2023-04-28 21:19:42 -0500

    Servo control via serial

--- RobotFiles/SerialComm/SerialComm.ino
+++ RobotFiles/SerialComm/SerialComm.ino
@@ -1,11 +1,32 @@
+#define dataSize 3
+
 void setup() {
   Serial.begin(9600);
 }
-int n = 0;
+
+byte data[dataSize] = {0,0,0};
 void loop() {
-  delay(1);
-  if (Serial.available())
-    n = Serial.read();
-  if (Serial.availableForWrite())
-    Serial.println(n);
+  delay(10);
+  // Discard all but the last bytes in buffer
+  while (Serial.available() > dataSize){
+    byte discard = Serial.read();
+  }
+  // If buffer is equal to data then transfer data
+  if (Serial.available() == dataSize){
+    for (size_t i = 0; i < dataSize; i++)
+    {
+      data[i] = Serial.read();
+    }      
+    printData();
+  }      
+}
+
+void printData(){
+  if (Serial.availableForWrite()){
+    for (size_t i = 0; i < dataSize; i++)
+    {
+      Serial.print(data[i]); Serial.print(" ");
+    }
+    Serial.println();      
+  }  
 }
