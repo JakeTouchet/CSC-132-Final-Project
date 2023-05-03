f72d38d212bc3cd9061df049f97158fe8da78788
JakeTouchet <jake.t.touchet@gmail.com>
2023-05-03 15:45:40 -0500

    change micro stuff

--- ObjectDetection/detect.py
+++ ObjectDetection/detect.py
@@ -98,7 +98,7 @@ def main(args):
 
                     # Turn robot to face object
                     if abs(x_dist) > TURN_THRESH:
-                        timedTurn(x_dist/5, speed=16)
+                        timedTurn(x_dist/3, speed=16)
                         # Set phase to micro adjusting (an object was detected)
                         micro_adjusting = True
 
