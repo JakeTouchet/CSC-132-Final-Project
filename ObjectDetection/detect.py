87987472bc3ae808a86b81dd67b98ffbc7f3945b
JakeTouchet <jake.t.touchet@gmail.com>
2023-05-04 17:33:11 -0500

    fix sign

--- ObjectDetection/detect.py
+++ ObjectDetection/detect.py
@@ -39,7 +39,7 @@ def main(args):
     GPIO.setup(bin_pin2, GPIO.OUT)
     GPIO.setup(bin_pin3, GPIO.OUT)
 
-    TURN_THRESH = 0.1
+    TURN_THRESH = 0.2
 
 
     # Connect to RabbitMQ ########################################################
@@ -106,7 +106,7 @@ def main(args):
 
                     # Turn robot to face object
                     if abs(x_dist) > TURN_THRESH:
-                        timedTurn(x_dist/5, speed=16)
+                        timedTurn(min(abs(x_dist), 0.5)/5*sign, speed=16)
                         # Set phase to micro adjusting (an object was detected)
                         micro_adjusting = True
                     else:
